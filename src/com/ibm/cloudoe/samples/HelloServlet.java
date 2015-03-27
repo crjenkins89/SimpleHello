34ddf0bda889f78edc20cca9fcb4821dca9c4f9a
Chris Jenkins <crjenkin@us.ibm.com>
2015-03-27 08:45:03 -0400

    Start to describe the dragon.

--- src/com/ibm/cloudoe/samples/HelloServlet.java
+++ src/com/ibm/cloudoe/samples/HelloServlet.java
@@ -16,7 +16,7 @@ public class HelloServlet extends HttpServlet {
         PrintWriter writer = response.getWriter();
         writer.println("Hello world from " + System.getenv("VCAP_APP_HOST") + ":" + System.getenv("VCAP_APP_PORT"));
         writer.println("");
-        writer.println("\tOnce upon a time there was a dragon.  Not a particularly large or ferocious dragon, but a dragon none the less.  This dragon happens to be the focus of our tale.  One would expect a dragon to have a name and this particular dragon's name was Gnarl.  While this might seem like a peculiar name for eithe ryou or me, this name was perfectly suitable for a dragon.");
+        writer.println("\tOnce upon a time there was a dragon.  Not a particularly large or ferocious dragon, but a dragon none the less.  This dragon happens to be the focus of our tale.  One would expect a dragon to have a name and this particular dragon's name was Gnarl.  While this might seem like a peculiar name for eithe ryou or me, this name was perfectly suitable for a dragon.\n\tNow you might be asking yourself, \"What does this dragon look like?\"  This dragon, Gnarl, was a dark shade of brown.  Not quite mud colored, but close.");
         writer.close();
     }
 }
